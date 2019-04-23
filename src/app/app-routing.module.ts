586cf38bb628fd37220fe53af4bfd8bdcf702825
José Rafael Alvarez Espino <jose.alvarez@atmira.com>
2019-04-23 08:55:08 +0200

    new view forgot-password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './views/login/pages/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'forgot_password',
+    loadChildren: './views/forgot-password/forgot-password.module#ForgotPasswordModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
