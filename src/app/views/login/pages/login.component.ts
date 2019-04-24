b4b242d64f65b3d726d22f62cbf2c4c1d7cf9c68
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-04-24 20:56:25 +0200

    Created login

--- src/app/views/login/pages/login.component.ts
+++ src/app/views/login/pages/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,11 @@ export class LoginComponent implements OnInit {
   hide = true;
 
   loginForm = this.fb.group({
-    email: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder, private snackBarService: SnackBarService) { }
+  constructor(private fb: FormBuilder, private snackBarService: SnackBarService, private loginService: LoginService) { }
 
   get email() {
     return this.loginForm.get('email');
@@ -39,6 +40,29 @@ export class LoginComponent implements OnInit {
       case 'PASSWORD_CHANGED':
         this.snackBarService.showSnackbar('Contraseña cambiada con éxito.', 2000, 'bottom', 'success');
         break;
+    }
+  }
+
+  login() {
+    if (this.loginForm.status === 'INVALID') {
+      return;
+    }
+    const email = this.email.value;
+    const password = this.password.value;
+    const json = {
+      email : email,
+      password : password
+    };
+    this.loginService.login(json).subscribe(this.loginSuccess.bind(this),
+    this.snackBarService.showSnackbar.bind(this, 'Error al conectar con el servidor', 1000, 'bottom', 'error'));
+  }
+
+  loginSuccess(response) {
+    if (!response) {
+      console.log('a');
+      this.snackBarService.showSnackbar('Email o contraseña incorrectos', 1500, 'bottom', 'error');
+    } else {
+      console.log(response);
     }
   }
 }
