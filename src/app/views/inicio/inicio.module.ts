063d74679c997f49b5a791cf3dfcd565354ec27e
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-05-06 14:33:04 +0200

    Primera subida

--- src/app/views/inicio/inicio.module.ts
+++ src/app/views/inicio/inicio.module.ts
@@ -5,12 +5,9 @@ import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './pages/inicio.component';
 import { IgniteModule } from 'src/app/shared/ignite/ignite.module';
 import { FormsModule } from '@angular/forms';
-import { SearchPersonalFileByDateComponent } from './components/search-personal-file-by-date/search-personal-file-by-date.component';
-import { SearchByEntryDateComponent } from './components/search-by-entry-date/search-by-entry-date.component';
-import { SearchByDepartureDateComponent } from './components/search-by-departure-date/search-by-departure-date.component';
 
 @NgModule({
-  declarations: [InicioComponent, SearchPersonalFileByDateComponent, SearchByEntryDateComponent, SearchByDepartureDateComponent],
+  declarations: [InicioComponent],
   imports: [
     CommonModule,
     InicioRoutingModule,
