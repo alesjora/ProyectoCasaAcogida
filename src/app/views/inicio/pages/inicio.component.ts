8b4dde580604fdf7e21d6638ea09e74782fe3c59
Jose Rafael <joserrafael.ja@gmail.com>
2019-05-12 13:22:26 +0200

    Updated

--- src/app/views/inicio/pages/inicio.component.ts
+++ src/app/views/inicio/pages/inicio.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { StoreService } from 'src/app/shared/services/store.service';
 import { LogoutService } from 'src/app/shared/services/logout.service';
+import { InicioService } from '../service/inicio.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 
 @Component({
@@ -12,30 +14,63 @@ export class InicioComponent implements OnInit {
   public search: string;
   public data: object[] = [];
   displayedColumns = ['name', 'entry_date', 'departure_date', 'room', 'bed'];
-  dataSource = ELEMENT_DATA;
+  public dataSource;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(public storeService: StoreService,
-    private logoutService: LogoutService) { }
+    private logoutService: LogoutService,
+    private inicioService: InicioService) { }
 
   ngOnInit() {
+    this.inicioService.getPersonasEnCasa().subscribe(this.getPersonasEnCasaSuccess.bind(this));
   }
-  mostrar() {
-    console.log(this.search);
+  getPersonasEnCasaSuccess(response) {
+    switch (response.status) {
+      case 'SESSION_EXPIRED':
+        this.logoutService.goToLoginWithMessage('SESSION_EXPIRED');
+        break;
+      case 'OPERATION_SUCCESS':
+        this.dataSource = new MatTableDataSource(this.generarData(response.data));
+        this.dataSource.sort = this.sort;
+        break;
+    }
   }
-  selectRow(row){
-    console.log(row);
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  generarData(data) {
+    let arrayData: Array<PersonElement> = [];
+    data.forEach(element => {
+      let search = element.name + " " + element.surname1 + " " + element.surname2 + " " + element.entry_date + " " + element.departure_date + " " + element.room + " " + element.bed;
+      arrayData.push({
+        image: element.image,
+        id: element.id,
+        name: element.name,
+        surname1: element.surname1,
+        surname2: element.surname2,
+        entry_date: element.entry_date,
+        departure_date: element.departure_date,
+        room: element.room,
+        bed: element.bed,
+        search
+      });
+    });
+    return arrayData;
   }
 }
 export interface PersonElement {
   image: string;
   id: string;
   name: string;
+  surname1: string;
+  surname2: string;
   entry_date: string;
   departure_date: string;
   room: string;
   bed: string;
+  search: string;
 }
 
-const ELEMENT_DATA: PersonElement[] = [
-  {image: 'http://localhost/api/public/image/StandarProfile.png', id: '1', name: 'José Rafael Álvarez Espino', entry_date: '04/05/2019', departure_date: '--/--/----', room: '3', bed: '4'},
-  {image: 'http://localhost/api/public/image/StandarProfile.png', id: '2', name: 'Marcos Gallardo Pérez', entry_date: '06/05/2019', departure_date: '--/--/----', room: '11', bed: '5'}
-];
+// const ELEMENT_DATA: PersonElement[] = [
+//   {image: 'http://localhost/api/public/image/StandarProfile.png', id: '1', name: 'José Rafael Álvarez Espino', entry_date: '04/05/2019', departure_date: '--/--/----', room: '3', bed: '4'},
+//   {image: 'http://localhost/api/public/image/StandarProfile.png', id: '2', name: 'Marcos Gallardo Pérez', entry_date: '06/05/2019', departure_date: '--/--/----', room: '11', bed: '5'}
+// ];
--- src/app/views/show-ocupation/pages/show-ocupation.component.ts
+++ src/app/views/show-ocupation/pages/show-ocupation.component.ts
@@ -10,80 +10,9 @@ import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
   styleUrls: ['./show-ocupation.component.scss']
 })
 export class ShowOcupationComponent implements OnInit {
-  // public occupation = {
-  //   status: 'OPERATION_SUCCESS',
-  //   data: [{
-  //     room: '1',
-  //     beds: [{
-  //       number: '1',
-  //       occupant: {
-  //         id: '1',
-  //         name: 'José Rafael',
-  //         surnames: 'Álvarez Espino',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     }, {
-  //       number: '2',
-  //       occupant: {
-  //         id: '2',
-  //         name: 'Marcos',
-  //         surnames: 'Gallardo Pérez',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     }, {
-  //       number: '3',
-  //       occupant: ''
-  //     }, {
-  //       number: '4',
-  //       occupant: {
-  //         id: '3',
-  //         name: 'Abdesaman',
-  //         surnames: 'Hammad El Habibi',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     }]
-  //   }, {
-  //     room: '2',
-  //     beds: [{
-  //       number: '1',
-  //       occupant: ''
-  //     }, {
-  //       number: '2',
-  //       occupant: {
-  //         id: '2',
-  //         name: 'Marcos',
-  //         surnames: 'Gallardo Pérez',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     }, {
-  //       number: '3',
-  //       occupant: ''
-  //     }, {
-  //       number: '4',
-  //       occupant: {
-  //         id: '3',
-  //         name: 'Abdesaman',
-  //         surnames: 'Hammad El Habibi',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     },
-  //     {
-  //       number: '5',
-  //       occupant: '',
-  //     }, {
-  //       number: '6',
-  //       occupant: {
-  //         id: '2',
-  //         name: 'Marcos',
-  //         surnames: 'Gallardo Pérez',
-  //         image: 'http://localhost/api/public/image/StandarProfile.png'
-  //       }
-  //     }]
-  //   }]
-  // };
+
   public occupation = [];
   public data: object[] = [];
-  public list = new Array(7);
   displayedColumns = ['n_cama', 'ocupante'];
   constructor(private storeService: StoreService,
     private logoutService: LogoutService, private showOccupationService: ShowOcupationService, private snackBarService: SnackBarService) {
