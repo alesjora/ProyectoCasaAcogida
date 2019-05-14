bcbf8549773be303fe6bee11d4f88cf751349bfd
manuel.gonzalez <manuel.gonzalez@atmira.com>
2019-05-14 14:09:46 +0200

    creando la vista personal

--- src/app/views/show-personal-file/pages/show-personal-file.component.ts
+++ src/app/views/show-personal-file/pages/show-personal-file.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ShowPersonalFileService } from '../service/show-personal-file.service';
 import { ActivatedRoute } from '@angular/router';
 import { LogoutService } from 'src/app/shared/services/logout.service';
 import { environment } from 'src/environments/environment';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-show-personal-file',
@@ -10,7 +11,12 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./show-personal-file.component.scss']
 })
 export class ShowPersonalFileComponent implements OnInit {
-  srcImagen: string;
+  displayedColumns: string[] = ['nRegistro', 'fechaEntrada', 'fechaSalida', 'habitacion', 'cama'];
+  fechasEntradaSalida = [];
+  dataSource = new MatTableDataSource(this.fechasEntradaSalida);
+
+  datosUltimoRegistro = {ultimaFechaIngreso: new Date(0), index: -1, dentroDeLaCasa: false};
+  srcImagen = '';
   nombre: string;
   apellido1: string;
   apellido2: string;
@@ -18,8 +24,8 @@ export class ShowPersonalFileComponent implements OnInit {
   paisNacimiento: string;
   fechaNacimiento: string;
   documentation: string;
-  email: string;
-  phone: string;
+
+  @ViewChild(MatSort) sort: MatSort;
 
   nHabitacion = 2;
   nCama = 3;
@@ -36,23 +42,47 @@ export class ShowPersonalFileComponent implements OnInit {
           this.logoutService.goToLoginWithMessage('SESSION_EXPIRED');
           break;
         case 'OPERATION_SUCCESS':
-          console.log(response.data);
           this.srcImagen = response.data.mainData[0].image;
           if (response.data.mainData[0].image === '') {
             this.srcImagen = environment.urlImage + 'StandarProfile.png';
           }
           this.nombre = response.data.mainData[0].nombre;
-          this.apellido1 = response.data.mainData[0].apellido1;
-          this.apellido2 = response.data.mainData[0].apellido2;
-          this.nacionalidad = response.data.mainData[0].nacionalidad;
-          this.paisNacimiento = response.data.mainData[0].lugarNacimiento;
-          this.fechaNacimiento = response.data.mainData[0].fecha_nacimiento;
+          this.apellido1 = response.data.mainData[0].apellido1 ? response.data.mainData[0].apellido1 : null;
+          this.apellido2 = response.data.mainData[0].apellido2 ? response.data.mainData[0].apellido2 : null ;
+          this.nacionalidad = response.data.mainData[0].nacionalidad ? response.data.mainData[0].nacionalidad : null;
+          this.paisNacimiento = response.data.mainData[0].lugarNacimiento ? response.data.mainData[0].lugarNacimiento : null;
+          this.fechaNacimiento = response.data.mainData[0].fecha_nacimiento ? response.data.mainData[0].fechaNacimiento : null;
+
+          response.data.mainData.forEach((element, index) => {
+            if (element.fecha_ingreso) {
+              const fechaEntrada = new Date(element.fecha_ingreso);
+              const fechaSalida = element.fecha_salida ? new Date(element.fecha_salida) : null;
+
+              this.fechasEntradaSalida.push({
+                                            nRegistro: '123',
+                                            FechaEntrada: this.formatoFecha(fechaEntrada),
+                                            FechaSalida : this.formatoFecha(fechaSalida),
+                                            habitacion: '1',
+                                            cama : '1'});
+              if (fechaEntrada.getTime() > this.datosUltimoRegistro.ultimaFechaIngreso.getTime()) {
+                this.datosUltimoRegistro.ultimaFechaIngreso = fechaEntrada;
+                this.datosUltimoRegistro.index = index;
+              }
+            }
+          });
+          console.log("datos registro", this.datosUltimoRegistro);
+          this.dataSource = new MatTableDataSource(this.fechasEntradaSalida);
+          this.dataSource.sort = this.sort;
           break;
         default:
           this.logoutService.goToLogin();
           break;
       }
     });
+  }
+  formatoFecha(date) {
+    return date != null ? date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() : '';
+
   }
 
 }
--- src/app/views/show-personal-file/show-personal-file.module.ts
+++ src/app/views/show-personal-file/show-personal-file.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ShowPersonalFileRoutingModule } from './show-personal-file-routing.module';
 import { ShowPersonalFileComponent } from './pages/show-personal-file.component';
-import { TableSortingComponent } from './components/table-sorting/table-sorting.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { IgniteModule } from 'src/app/shared/ignite/ignite.module';
 
 @NgModule({
-  declarations: [ShowPersonalFileComponent, TableSortingComponent],
+  declarations: [ShowPersonalFileComponent],
   imports: [
     CommonModule,
     ShowPersonalFileRoutingModule,
