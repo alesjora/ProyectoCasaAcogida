ff8efe546c92614ac8b5a86ca7b8a964def483a2
José Rafael Alvarez Espino <jose.alvarez@atmira.com>
2019-05-14 13:41:55 +0200

    Updated

--- src/app/views/inicio/inicio.module.ts
+++ src/app/views/inicio/inicio.module.ts
@@ -7,9 +7,10 @@ import { IgniteModule } from 'src/app/shared/ignite/ignite.module';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { TablePeopleAtHomeComponent } from './components/table-people-at-home/table-people-at-home.component';
+import { TaulaPersonesExpedientsComponent } from './components/taula-persones-expedients/taula-persones-expedients.component';
 
 @NgModule({
-  declarations: [InicioComponent, TablePeopleAtHomeComponent],
+  declarations: [InicioComponent, TablePeopleAtHomeComponent, TaulaPersonesExpedientsComponent],
   imports: [
     CommonModule,
     InicioRoutingModule,
