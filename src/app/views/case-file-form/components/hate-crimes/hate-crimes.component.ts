9821aee89d34bc1722156e17918226f96e9ec1ba
Marcos <marcosgallardoperez@gmail.com>
2019-05-27 22:30:09 +0200

    formulario gordo

--- src/app/views/case-file-form/components/hate-crimes/hate-crimes.component.ts
+++ src/app/views/case-file-form/components/hate-crimes/hate-crimes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { StayService } from 'src/app/shared/services/stay.service';
 import { LogoutService } from 'src/app/shared/services/logout.service';
 import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
@@ -11,47 +11,63 @@ import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
 })
 export class HateCrimesComponent implements OnInit {
 
+  hateCrimesForm;
+  afirmacionNegacion = [{value: 1, viewValue: 'Si'}, {value: 2, viewValue: 'No'}];
   tiposViolenciaSeleccionables = [];
   lugaresAgresionSeleccionables = [];
   momentosAgresionSeleccionables = [];
   consecuenciasFisicasAgresionSeleccionables = [];
   consecuenciasPicologicasAgresionSeleccionables = [];
   consecuenciasSocioeconomicasAgresionSeleccionables = [];
+  generoAgresorSeleccionable = [{value: '1' , viewValue: 'Hombre'}, {value: '2', viewValue: 'Mujer'}];
+
+  public selectedSufridoViolencia: string;
+  public selectedElementoIntimidatorio: string;
+
   constructor(private fb: FormBuilder,
               private stayService: StayService,
               private logoutService: LogoutService,
               private snackBarService: SnackBarService) { }
-  hateCrimesForm;
-  tiposDocumento = [{value: '1', viewValue: 'holaCavesa'}];
 
-  afirmacionNegacion = [{value: 1, viewValue: 'Si'}, {value: 2, viewValue: 'No'}];
-
-  public selectedSufridoViolencia: string;
   ngOnInit() {
     this.createForm();
     this.getDatos();
   }
+
   createForm() {
     this.hateCrimesForm = this.fb.group({
       sufridoViolencia: [''],
-      documentation: this.fb.array([
+      detallesAgresion: this.fb.array([
         this.fb.group({
-          documentionType: new FormControl(''),
-          documentationOtherType: new FormControl(''),
-          documentationNumber: new FormControl('')
+          agressionType: new FormControl(''),
+          agressionPlace: new FormControl(''),
+          agresionPlaceTypology: new FormControl(''),
+          agressionMoment: new FormControl(''),
+          agresores: this.fb.array([
+            this.fb.group({
+              agressorGlender: new FormControl(''),
+              agressorAge: new FormControl(''),
+              intimidatoryObjects: new FormControl(''),
+            }),
+          ]),
         })
       ]),
     });
   }
+
   public formIsValid() {
     return this.hateCrimesForm.valid;
   }
-  get documentation() {
-    return this.hateCrimesForm.get('documentation') as FormArray;
+
+  get detallesAgresion() {
+    return this.hateCrimesForm.get('detallesAgresion') as FormArray;
   }
+
   get sufridoViolencia(){
     return this.hateCrimesForm.get('sufridoViolencia');
   }
+
+
   getDatos() {
     const serv = this.stayService;
     serv.getViolenceTypes().subscribe(this.peticionHandleMejorada
@@ -59,7 +75,7 @@ export class HateCrimesComponent implements OnInit {
     serv.getAgressionPlaces().subscribe(this.peticionHandleMejorada
       .bind(this, this.lugaresAgresionSeleccionables, 'id', 'lugar_agresion'));
     serv.getAgressionMoments().subscribe(this.peticionHandleMejorada
-      .bind(this, this.momentosAgresionSeleccionables, 'id', 't_momentos_agresion'));
+      .bind(this, this.momentosAgresionSeleccionables, 'id', 'momento_agresion'));
 
     serv.getPhysicalAgressionConsequences().subscribe(this.peticionHandleMejorada
       .bind(this, this.consecuenciasFisicasAgresionSeleccionables, 'id', 'consecuencia'));
@@ -68,28 +84,57 @@ export class HateCrimesComponent implements OnInit {
     serv.getSocioeconomicAgressionConsequences().subscribe(this.peticionHandleMejorada
       .bind(this, this.consecuenciasSocioeconomicasAgresionSeleccionables, 'id', 'consecuencia'));
 
-    console.log('fisica', this.consecuenciasFisicasAgresionSeleccionables);
-    console.log('psicofisica', this.consecuenciasPicologicasAgresionSeleccionables);
-    console.log('socioeconofisica', this.consecuenciasSocioeconomicasAgresionSeleccionables);
-
-
   }
-  addDocument() {
-    this.documentation.push(this.fb.group({
-      documentionType: '',
-      documentationOtherType: '',
-      documentationNumber: ''
+
+  addAgression() {
+    this.detallesAgresion.push(this.fb.group({
+      agressionType: '',
+      agressionPlace: '',
+      agresionPlaceTypology: '',
+      agressionMoment: '',
+      agresores: this.fb.array([
+        this.fb.group({
+          agressorGlender: new FormControl(''),
+          agressorAge: new FormControl(''),
+          intimidatoryObjects: new FormControl(''),
+        }),
+      ]),
     }));
   }
-  deleteDocument() {
-    this.documentation.removeAt(this.documentation.length-1);
+  addAgresor(index) {
+    const formulario = this.detallesAgresion.controls[index] as FormGroup;
+    const agresores = formulario.controls.agresores as FormArray;
+    agresores.push(
+      this.fb.group({
+        agressorGlender: '',
+        agressorAge: '',
+        intimidatoryObjects: '',
+      }));
   }
-  getDocumentation(index) {
-    //console.log(this.identifyingDataForm.get('documentation').value[index]);
-    return this.hateCrimesForm.get('documentation').value[index];
+
+  deleteAgression() {
+    this.detallesAgresion.removeAt(this.detallesAgresion.length - 1);
   }
+  deleteAgresor(index){
+    const formulario = this.detallesAgresion.controls[index] as FormGroup;
+    const agresores = formulario.controls.agresores as FormArray;
+    agresores.removeAt(
+      agresores.length - 1);
+  }
+
+  getDetallesAgresion(index) {
+    // console.log(this.hateCrimesForm.get('detallesAgresion').value[index]);
+    return this.hateCrimesForm.get('detallesAgresion').value[index];
+  }
+  getAgresores(index){
+    // console.log(this.hateCrimesForm.get('detallesAgresion').value[index].agresores);
+    const formulario = this.detallesAgresion.controls[index] as FormGroup;
+    return formulario.controls.agresores as FormArray;
+  }
+
+
   sendDatos(){
-    console.log(this.hateCrimesForm.value);
+    console.log("resultado",this.hateCrimesForm.value);
   }
   /**
    * Maneja la petición http
